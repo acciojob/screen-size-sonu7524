15c7914c16bb3587335b7fa8c194a6ceb48975db
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-04 11:02:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// define a function to get the width and height of the window
+function getWindowSize() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  // get the element with id "size" and update its text content with the current window size
+  document.getElementById("size").textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// add a resize event listener to the window object
+window.addEventListener('resize', getWindowSize);
+
+// call the getWindowSize function once initially to get the initial size of the window and display it on the page
+getWindowSize();
